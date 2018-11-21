03cfeb10e4a78df5c05db6666aae51cc2ac92ad6
bashir <billowbashir@gmail.com>
2018-11-21 08:23:51 +0300

    finish

--- smart.py
+++ smart.py
@@ -4,44 +4,43 @@ print "testing"
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.IN)
 GPIO.setup(17,GPIO.OUT)
+# works with both python 2 and 3
+from __future__ import print_function
 
+import africastalking
 
-# class SMS:
-#     def __init__(self):
-# 		# Set your app credentials
-# 	    self.username = "YOUR_USERNAME"
-#         self.api_key = "YOUR_API_KEY"
-# 		# Initialize the SDK
-#         africastalking.initialize(self.username, self.api_key)
-# 		# Get the SMS service
-#         self.sms = africastalking.SMS
+class SMS:
+    def __init__(self):
+        # Set your app credentials
+        self.username = "brighton"
+        self.api_key = "18696560f647f921b072950f4ef9e5c24a76e583ec26b6ceb4f2ef6d34403daa"
+        # Initialize the SDK
+        africastalking.initialize(self.username,self.api_key)
+        # Get the SMS service
+        self.sms = africastalking.SMS
 
-#     def send_sms_sync(self):
-#         # Set the numbers you want to send to in international format
-#         recipients = ["+254713YYYZZZ", "+254733YYYZZZ"]
-#         # Set your message
-#         message = "I'm a lumberjack and it's ok, I sleep all night and I work all day";
-#         # And send the SMS
-#         try:
-# 			# That’s it, hit send and we’ll take care of the rest
-#             response = self.sms.send(message, recipients)
-#             print (response)
-#         except Exception as e:
-#             print ('Encountered an error while sending: %s' % str(e))
-while True:
-   input_value = GPIO.input(21)
-   print input_value
-    if input_value==1:
-        GPIO.output(17,GPIO.HIGH)
-        # low moisture,watering
-    else:
-        GPIO.output(17,GPIO.LOW)
-        # enough moisture ,not watering
+    def send_sms_sync(self,message):
+        # Set the numbers you want to send to in international format
+        recipients = ["+254728683438","+254783285164"]
+        # Set your message
 
+        # And send the SMS
+        try:
+			# That’s it, hit send and we’ll take care of the rest
+            response = self.sms.send(message, recipients)
+            print (response)
+        except Exception as e:
+            print ('Encountered an error while sending: %s' % str(e))
+    def smart(self):
+        input_value = GPIO.input(21)
+        while True:
+            if input_value==1:
+                GPIO.output(17,GPIO.HIGH)
+                SMS().send_sms_sync('moisture is not enough, turning on pump')
 
+            else:
+                GPIO.output(17,GPIO.LOW)
+                SMS().send_sms_sync('moisture is enough turning off pump')
 
-
-
-
-# if __name__ == '__main__':
-#     SMS().send_sms_sync()       
+if __name__ == '__main__':
+    SMS().smart()
